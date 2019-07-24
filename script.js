b95e6d1a966a1f022da382bacfe6f205db112637
rogerdavid2 <rogerb7@uw.edu>
2019-07-24 10:23:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+document.getElementById("entire-form").addEventListener("submit", saveTask);
+
+function saveTask(e) {
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+
+    const task = {
+        title,
+        description
+    };
+
+    if (localStorage.getItem("tasks") === null) {
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else {
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    //document.getElementById("entire-form").reset();
+    getTasks();
+    e.preventDefault();
+}
+
+
+function getTasks() {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let viewTask = document.getElementById("tasks");
+
+    viewTask.innerHTML = '';
+
+    for (let i = 0; i < tasks.length; i++) {
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+        viewTask.innerHTML += `<div class ="card with-margin">
+            <div class ="card-body">
+            <p>${title} - ${description}</p>
+            <a class ="delete" onclick="deleteTask('${title}')">Delete</a>
+            </div>
+       </div >`
+    }
+}
+
+getTasks();
+
+
+/*function deleteTask(title) {
+    let tasks = JSON.stringify(localStorage.getItem("tasks"));
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    getTasks();
+}*/
